63c41f6b016d52e20eed47ccbd64e9bf6144bee7
Nikola Milosevic <nikola.milosevic86@gmail.com>
2016-04-14 22:18:37 +0100

    New dataset and fix for the ballancing file

--- Balance.py
+++ Balance.py
@@ -10,7 +10,7 @@ import random
 with open('learnngCellDatasetWClassPatientNum.csv') as f:
     content = f.readlines()
     f2 = open('learnngCellDatasetBalanced.csv', 'w+')
-    f2.write("ArticleId,PMCid,TableName,SpecPragmatics,CellContent,Header,Stub,SuperRow,rowN,columnN,function\n")
+    f2.write("ArticleId,PMCid,TableName,SpecPragmatics,CellContent,Header,Stub,SuperRow,rowN,columnN,function,hasValue\n")
     i = 0
     countbad = 0
     countgood = 0
@@ -19,15 +19,15 @@ with open('learnngCellDatasetWClassPatientNum.csv') as f:
         row = content[i]
         last = row.split(",")[11]
         print last
-        if('yes' in last and countgood<142):
+        if('yes' in last and countgood<182):
             f2.write(row)
             countgood = countgood + 1
         else:
             NoArray.append(row)
         i = i+1
     i=0
-    random.seed(126)
-    while i < 2000:
+    random.seed(419)
+    while i < 200:
 
         randNum = random.randrange(0,len(NoArray))
         f2.write(NoArray[randNum])

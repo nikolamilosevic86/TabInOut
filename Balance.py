e4e486d5623b733250378c0df01520e1484e768b
Nikola Milosevic <nikola.milosevic86@gmail.com>
2016-05-06 09:26:15 +0100

    data

--- Balance.py
+++ Balance.py
@@ -7,9 +7,9 @@ Created at the University of Manchester, School of Computer Science
 Licence GNU/GPL 3.0
 '''
 import random
-with open('learnngCellDatasetWClassPatientNum.csv') as f:
+with open('learnngCellDatasetAge.csv') as f:
     content = f.readlines()
-    f2 = open('learnngCellDatasetBalanced.csv', 'w+')
+    f2 = open('learnngCellDatasetAgeBalanced.csv', 'w+')
     f2.write("ArticleId,PMCid,TableName,SpecPragmatics,CellContent,Header,Stub,SuperRow,rowN,columnN,function,hasValue\n")
     i = 0
     countbad = 0
@@ -17,15 +17,16 @@ with open('learnngCellDatasetWClassPatientNum.csv') as f:
     NoArray = []
     while i<len(content):
         row = content[i]
-        last = row.split(",")[11]
-        print last
-        if('yes' in last and countgood<182):
+        split = row.split(",")
+        last = split[len(split)-1]
+        if('yes' in last):
             f2.write(row)
             countgood = countgood + 1
         else:
             NoArray.append(row)
         i = i+1
     i=0
+    print countgood
     random.seed(419)
     while i < 200:
 

31b9a1fde97eaeef7b45cd97b906d9803123f103
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2016-06-01 16:47:48 +0100

    Added new GUI function

--- TkGUIFirstScreen.py
+++ TkGUIFirstScreen.py
@@ -7,6 +7,80 @@ Created at the University of Manchester, School of Computer Science
 Licence GNU/GPL 3.0
 '''
 from Tkinter import *
+
+def RemoveRule():
+    try:
+        # get selected line index
+        index = Lb1.curselection()[0]
+        Lb1.delete(index)
+    except IndexError:
+        pass
+
+def MoveRuleUp():
+    try:
+        pos = Lb1.curselection()[0]
+        # get selected line index
+        if pos == 0:
+            return
+
+        text = Lb1.get(pos)
+        Lb1.delete(pos)
+        Lb1.insert(pos-1, text)
+    except IndexError:
+        pass
+
+def MoveRuleDown():
+    try:
+        pos = Lb1.curselection()[0]
+        # get selected line index
+        if pos == Lb1.size():
+            return
+
+        text = Lb1.get(pos)
+        Lb1.delete(pos)
+        Lb1.insert(pos+1, text)
+    except IndexError:
+        pass
+
+def AddEditRule():
+    add = Tk()
+    add.title("Add/Edit Rule")
+    add.geometry('{}x{}'.format(200, 200))
+    itemsFrame = Frame(add,height=130)
+    itemsFrame.pack()
+    leftFrame = Frame(itemsFrame,height=130)
+    leftFrame.pack(side=LEFT)
+    rightFrame = Frame(itemsFrame,height=130)
+    rightFrame.pack(side=LEFT)
+    editWhiteList = Button(leftFrame,text="Edit White List")
+    editWhiteList.pack()
+    editBlackList = Button(leftFrame,text="Edit White List")
+    editBlackList.pack()
+    name = StringVar()
+    label_name = Label(frame,textvariable=name)
+    name.set("Name of task:")
+    label_name.pack()
+    HeaderCB = Checkbutton(rightFrame,text="Header").grid(row=0,sticky='w')
+    StubCB = Checkbutton(rightFrame,text="Stub").grid(row=1,sticky='w')
+    SuperRowCB = Checkbutton(rightFrame,text="Super-row").grid(row=2,sticky='w')
+    DataCB = Checkbutton(rightFrame,text="Data").grid(row=3,sticky='w')
+    EverywhereCB = Checkbutton(rightFrame,text="Everywhere").grid(row=4,sticky='w')
+    bottomframe2 = Frame(add,height=1)
+    bottomframe2.pack(side=BOTTOM)
+    bottomframe = Frame(add,height=40)
+    bottomframe.pack(side=BOTTOM)
+    save = Button(bottomframe, text="Save", fg="black")
+    save.pack()
+    
+
+def AddRule():
+    AddEditRule()
+    pass
+
+def EditRule():
+    AddEditRule()
+    pass
+
 top = Tk()
 top.title("Table InfExtractor")
 top.geometry('{}x{}'.format(500, 500))
@@ -47,15 +121,15 @@ Lb1.pack()
 
 
 
-AddRule = Button(bottomframe, text="Add Rule", fg="black")
+AddRule = Button(bottomframe, text="Add Rule", fg="black",command=AddRule)
 AddRule.pack( side = LEFT)
-DeleteRule = Button(bottomframe, text="Delete Rule", fg="black")
+DeleteRule = Button(bottomframe, text="Delete Rule", fg="black",command=RemoveRule)
 DeleteRule.pack( side = LEFT)
-EditRule = Button(bottomframe, text="Edit Rule", fg="black")
+EditRule = Button(bottomframe, text="Edit Rule", fg="black",command=AddRule)
 EditRule.pack( side = LEFT)
-MoveUpRule = Button(bottomframe, text="Move Up Rule", fg="black")
+MoveUpRule = Button(bottomframe, text="Move Up Rule", fg="black",command=MoveRuleUp)
 MoveUpRule.pack( side = LEFT)
-MoveDownRule = Button(bottomframe, text="Move Down Rule", fg="black")
+MoveDownRule = Button(bottomframe, text="Move Down Rule", fg="black",command=MoveRuleDown)
 MoveDownRule.pack( side = LEFT)
 Next = Button(bottomframe, text="Next", bg="green")
 Next.pack( side = LEFT)

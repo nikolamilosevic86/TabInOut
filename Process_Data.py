6875dd530ea88373a06a6efcabe02605cd861a81
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2016-06-13 17:13:38 +0100

    Started processing data

--- Process_Data.py
+++ Process_Data.py
@@ -6,8 +6,10 @@ Created on 13 Jun 2016
 Created at the University of Manchester, School of Computer Science
 Licence GNU/GPL 3.0
 '''
-
+import FileManipulationHelper
+import QueryDBClass
 
 def ProcessDataBase(project_name,rules):
-    
+    DBSettings = FileManipulationHelper.LoadDBConfigFile(project_name)
+    db = QueryDBClass.QueryDBCalss(DBSettings['Host'],DBSettings['User'],DBSettings['Pass'],DBSettings['Database'])
     pass

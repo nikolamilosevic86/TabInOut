5679da972a059bcba87e0dc69ef37741cb2d2e47
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2016-06-15 16:07:59 +0100

    Regex checks whether cell is valid candidate

--- Process_Data.py
+++ Process_Data.py
@@ -8,6 +8,56 @@ Licence GNU/GPL 3.0
 '''
 import FileManipulationHelper
 import QueryDBClass
+import re
+
+def CheckWListUsingRegex(look_header,look_stub,look_superrow,look_data,List,Header,Stub,Super_row,Data):
+    ContainsValue = False
+    if(look_header):
+        for item in List:
+            m1 = re.search('\\b('+item+")\\b",Header)
+            if(m1!=None):
+                ContainsValue = True
+    if(look_stub):
+        for item in List:
+            m1 = re.search('\\b('+item+")\\b",Stub)
+            if(m1!=None):
+                ContainsValue = True
+    if(look_superrow):
+        for item in List:
+            m1 = re.search('\\b('+item+")\\b",Super_row)
+            if(m1!=None):
+                ContainsValue = True
+    if(look_data):
+        for item in List:
+            m1 = re.search('\\b('+item+")\\b",Data)
+            if(m1!=None):
+                ContainsValue = True
+    return ContainsValue
+
+def CheckBListUsingRegex(look_header,look_stub,look_superrow,look_data,List,Header,Stub,Super_row,Data):
+    ValidCandidate = True
+    if(look_header):
+        for item in List:
+            m1 = re.search('\\b('+item+")\\b",Header)
+            if(m1!=None):
+                ValidCandidate = False
+    if(look_stub):
+        for item in List:
+            m1 = re.search('\\b('+item+")\\b",Stub)
+            if(m1!=None):
+                ValidCandidate = False
+    if(look_superrow):
+        for item in List:
+            m1 = re.search('\\b('+item+")\\b",Super_row)
+            if(m1!=None):
+                ValidCandidate = False
+    if(look_data):
+        for item in List:
+            m1 = re.search('\\b('+item+")\\b",Data)
+            if(m1!=None):
+                ValidCandidate = False
+    
+    return ValidCandidate
 
 def ProcessDataBase(project_name,rules):
     DBSettings = FileManipulationHelper.LoadDBConfigFile(project_name)
@@ -41,9 +91,13 @@ def ProcessDataBase(project_name,rules):
             print Header
             print Stub
             print Super_row
+            ContainsLooked = CheckWListUsingRegex(rule.look_header,rule.look_stub,rule.look_superrow,rule.look_data,rule.WhiteList,Header,Stub,Super_row,Content)
+            if(ContainsLooked):
+                ValidCandidate = CheckBListUsingRegex(rule.look_header,rule.look_stub,rule.look_superrow,rule.look_data,rule.WhiteList,Header,Stub,Super_row,Content)
+                if ValidCandidate:
+                    #Sintactic rules processing
+                    pass
             
             
-            
-            break
     pass
 

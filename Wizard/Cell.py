7f174dd81368ec7ccb74b9ac36ec67a36f1e88bc
nikola.milosevic86@gmail.com <nikola.milosevic86@gmail.com>
2016-11-10 17:28:15 +0000

    Cell structure.

---
+++ Wizard/Cell.py
@@ -0,0 +1,30 @@
+'''
+Created on 10 Nov 2016
+
+@author: mbaxkhm4
+
+Created at the University of Manchester, School of Computer Science
+Licence GNU/GPL 3.0
+'''
+class Cell:
+    idArticle = -1
+    idPMC = -1
+    idTable = -1
+    tableOrder = -1
+    pragmaticClass = ''
+    idCell = 0
+    cellType = ""
+    rowN = -1
+    columnN = -1
+    Content = ""
+    Header = ""
+    Stub = ""
+    Super_row = ""
+    HeaderId = -1
+    StubId = -1
+    SuperRowId = -1
+    isHeader = False
+    isStub = False
+    isSuperRow = False
+    isData = False
+    extractedBecauseOfAnnotations = False
